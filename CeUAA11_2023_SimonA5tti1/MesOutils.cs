fbda69fb02cae01fe92066d4f246b237c52464aa
Archambeau Simon <210303@CCM-Asty-Moulin.local>
2023-06-01 16:17:38 +0200

    first commit

---
+++ CeUAA11_2023_SimonA5tti1/MesOutils.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CeUAA11_2023_SimonA5tti1
+{
+    public struct MesOutils
+    {
+        public void matVigenere(String[] args)
+        {
+
+            string matVigenere = new string[4, phClaire.Length];
+            int j;
+            int[] phClaire;
+            int[] phClef;
+            j = 0;
+            for (int i = 0; i <= phClaire.Length - 1; i += (1))
+            {
+                matVigenere[0, 1] = phClaire[i];
+                if (phClef.GetLength())
+                {
+                    j = 0;
+                }
+                matVigenere[1, i] = phClef[j];
+                matVigenere[2, i] = ((int)phClef[j]) - 65.ToString();
+                if ((int)phClaire[i] + int.Parse(matVigenere[2, 1]) <= 90)
+                {
+                    codeAscii = (int)char.Parse(matVigenere[0, i]) + int.Parse(matVigenere[2, i]);
+                }
+                else
+                {
+                    codeAscii = (int)char.Parse(matVigenere[0, i]) + int.Parse(matVigenere[2, i]) - 26;
+                }
+                matVigenere[3, i] = Convert.ToChar(code.Ascii).ToString();
+                j = j + 1;
+            }
+        }
+
+        public static void matAffine(String[] args out)
+        {
+
+            int[,] matAffine = new int;
+            string a;
+            string b;
+            int[,] phClaire;
+            matAffine = new string[4, phClaire.GetLength()];
+            for (int i = 0; i <= phClaire.GetLength() - 1; i += (1))
+            {
+             matAffine[0, 1] = phClaire[i];
+
+             int x = phClaire[i] - 65;
+             matAffine[1, i] = x;
+
+             int y = (a * x + b) % 26;
+             matAffine[2, 1] = y;
+             matAffine[3, 1] = y + 65;
+            }
+        }
+    }
+}
---
+++ CeUAA11_2023_SimonA5tti1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace CeUAA11_2023_SimonA5tti1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MesOutils MesOutils = new MesOutils();
+            bool suivi = true;
+            while (suivi)
+            {
+                Console.WriteLine("Bienvenue dans mon programme de Matrice !");
+                Console.WriteLine("Tapez 1 pour crypter une matrice avec la manière de Vigenère");
+                Console.WriteLine("Tapez 2 pour crypter une matrice avec la méthode du chiffre affine");
+                Console.WriteLine("Tapez 3 pour quitter");
+                String choix = Console.ReadLine();
+                {
+                    case "1":            
+                        string matVigenere;
+                        int j;
+                        int[] phClaire;
+                        int[] phClef;
+                        Console.Write("Veuillez saisir une phrase a crypter");
+                        int phClaire = Console.ReadLine();
+                        Console.WriteLine(phClaire);
+                    OutOfMemoryException matVigenere;
+                    break;
+                    case "2":
+                        String a = "";
+                        String b = "";
+                        int[,] x;
+                        int[,] y;
+                        int[,] phClaire;
+                        Console.Write("Veuillez saisir un chiffre impaire entre 0 et 25 (13 exclu)");
+                        String a = Console.ReadLine();
+                        Console.WriteLine(a);
+                        Console.Write("Veuillez saisir un chiffre entre 0 et 25");
+                        String b = Console.ReadLine();
+                        Console.WriteLine(b);
+                        MesOutils.matAffine(String[] args out);
+                        break;
+                    case "3":
+                        suivi = false;
+                        break;
+                    default:
+                        Console.WriteLine("Option non valide");
+                        break;
+                }
+            }
+            Console.WriteLine("Appuyez sur une touche pour quitter...");
+            Console.ReadKey();
+        }
+    }
+}
